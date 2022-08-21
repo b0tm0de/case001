9c4450a1111473f4fbbf1e6d55092e4f2a408136
Turgut Sezer <hackcity@gmail.com>
2022-08-21 03:59:51 +0300

    added cursor, font and background

--- src/Components/Cell.jsx
+++ src/Components/Cell.jsx
@@ -1,3 +1,11 @@
-export default function Cell({ id, sign }) {
-  return <div className="cell"></div>
+export default function Cell({ id, sign, handleClick }) {
+  return (
+    <div
+      onClick={() => handleClick(id)}
+      className="cell"
+      style={{ cursor: sign ? "not-allowed" : "url('../cursor.cur'), auto" }}
+    >
+      <span className="cell-sign">{sign}</span>
+    </div>
+  )
 }
