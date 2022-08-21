01bf41647119e988530a07b39d4e1c3092c3e5af
Turgut Sezer <hackcity@gmail.com>
2022-08-21 19:26:45 +0300

    started for winner check function

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,57 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Cell from "./Components/Cell"
 
 function App() {
-  const defaultBoard = new Array(9).fill(null)
-  const [board, setBoard] = useState(defaultBoard)
+  const emptyBoard = Array(9).fill("")
+  const [board, setBoard] = useState(emptyBoard)
   const [turn, setTurn] = useState("X")
 
+  function checkForWinner(turn) {
+    const firstRow = board[0] === board[1] && board[0] === board[2]
+    const secondRow = board[3] === board[4] && board[3] === board[5]
+    const thirdRow = board[6] === board[7] && board[6] === board[8]
+    const horizontalWin = [firstRow, secondRow, thirdRow].some((elem) => elem === true)
+
+    const firstCol = board[0] === board[3] && board[0] === board[6]
+    const secondCol = board[1] === board[4] && board[1] === board[7]
+    const thirdCol = board[2] === board[5] && board[2] === board[8]
+    const verticalWin = [firstCol, secondCol, thirdCol].some((elem) => elem === true)
+
+    const firstCross = board[0] === board[4] && board[0] === board[8]
+    const secondCross = board[2] === board[4] && board[2] === board[6]
+    const diagonalWin = [firstCross, secondCross].some((elem) => elem === true)
+
+    if (diagonalWin || verticalWin || horizontalWin) {
+      console.log("Winner: " + turn)
+    }
+  }
+
   function handleClick(id) {
-    if (board[id]) return
+    const targetCell = board[id]
+
+    // return if target cell is not null (contains X / O)
+    if (targetCell) return
+
     setBoard((prevBoard) => {
       const newBoard = [...prevBoard]
       newBoard[id] = turn
       return newBoard
     })
-
-    setTurn((prevTurn) => (prevTurn === "X" ? "O" : "X"))
   }
 
-  const boardMapped = board.map((sign, i) => {
+  useEffect(() => {
+    setTurn((prevTurn) => (prevTurn === "X" ? "O" : "X"))
+    checkForWinner(turn)
+  }, [board])
+
+  const boardElements = board.map((sign, i) => {
     return <Cell key={i} id={i} sign={sign} setBoard={setBoard} handleClick={handleClick} />
   })
 
   return (
     <main className="app">
       <span>{turn}'s turn.</span>
-      <div className="game-grid">{boardMapped}</div>
+      <div className="game-grid">{boardElements}</div>
 
       <details className="fun-fact">
         <summary>Fun fact:</summary>
--- src/Components/Cell.jsx
+++ src/Components/Cell.jsx
@@ -1,12 +1,14 @@
 import handPenCursor from "../assets/hand-pen-cursor.png"
 
 export default function Cell({ id, sign, handleClick }) {
+  const style = {
+    // App > handleClick() preventing clicks if necessary, here only for visuals
+    cursor: sign ? "not-allowed" : `url(${handPenCursor}), auto`,
+    color: sign === "X" ? "var(--color-pencil-blue)" : "var(--color-pencil-red)",
+  }
+
   return (
-    <div
-      onClick={() => handleClick(id)}
-      className="cell"
-      style={{ cursor: sign ? "not-allowed" : `url(${handPenCursor}), auto` }}
-    >
+    <div onClick={() => handleClick(id)} className="cell" style={style}>
       <span className="cell-sign">{sign}</span>
     </div>
   )
