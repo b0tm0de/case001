805992e9e813bb540fa83f392a6047ebcfe87ff6
Turgut Sezer <hackcity@gmail.com>
2023-04-06 09:41:28 +0300

    refactored and simplified winner checking logic

--- src/helpers/checkForWinner.js
+++ src/helpers/checkForWinner.js
@@ -1,22 +1,21 @@
 export default function checkForWinner(board, turn, setWinnerMessage, setIsGameOver) {
-  const firstRow = [board[0], board[1], board[2]].every((el) => el === turn)
-  const secondRow = [board[3], board[4], board[5]].every((el) => el === turn)
-  const thirdRow = [board[6], board[7], board[8]].every((el) => el === turn)
+  const winningPositions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
 
-  const horizontalWin = [firstRow, secondRow, thirdRow].some((el) => el === true)
+  const winnerFound = winningPositions.some((position) => {
+    const [a, b, c] = position
+    return board[a] === turn && board[b] === turn && board[c] === turn
+  })
 
-  const firstCol = [board[0], board[3], board[6]].every((el) => el === turn)
-  const secondCol = [board[1], board[4], board[7]].every((el) => el === turn)
-  const thirdCol = [board[2], board[5], board[8]].every((el) => el === turn)
-
-  const verticalWin = [firstCol, secondCol, thirdCol].some((el) => el === true)
-
-  const firstCross = [board[0], board[4], board[8]].every((el) => el === turn)
-  const secondCross = [board[2], board[4], board[6]].every((el) => el === turn)
-
-  const diagonalWin = [firstCross, secondCross].some((el) => el === true)
-
-  if (horizontalWin || verticalWin || diagonalWin) {
+  if (winnerFound) {
     setWinnerMessage(`${turn} won the game.`)
     setIsGameOver(true)
     return
@@ -24,7 +23,7 @@ export default function checkForWinner(board, turn, setWinnerMessage, setIsGameO
 
   // if there is no undefined cell, means all cells filled
   // and still there is no win condition, so it is a tie
-  if (!board.some((el) => el === undefined)) {
+  if (!board.includes(undefined)) {
     setWinnerMessage("Tie")
     setIsGameOver(true)
     return
